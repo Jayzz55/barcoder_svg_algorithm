38d3d860ffafc59226e2668b068bd1eccd09cb3a
Jaya Wijono from Dot Kingdom <jaya@hhd.com.au>
2015-11-30 23:33:51 +1100

    add build svg function

---
+++ lib/barcoder_svg_algorithm/svg_builder.ex
@@ -0,0 +1,29 @@
+defmodule BarcoderSvgAlgorithm.SvgBuilder do
+  import BarcoderSvgAlgorithm.Group
+
+  @doc """
+  test
+  ## Examples
+      iex> BarcoderSvgAlgorithm.SvgBuilder.path([1,0,0,1,1])
+      "v10h1V1h2v10h2V1"
+  """
+  def path(list) do
+    grouped_list = group(list)
+    build_path(grouped_list, "")
+  end
+  defp build_path([], acc), do: acc
+  defp build_path([ {1, width} | tail ], acc), do: build_path(tail, acc <> "v10h#{width}V1")
+  defp build_path([ {0, width} | tail ], acc), do: build_path(tail, acc <> "h#{width}")
+
+  @doc """
+  test
+  ## Examples
+      iex> BarcoderSvgAlgorithm.SvgBuilder.build_svg([1,0,0,1,1])
+      "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 100'><path d='M1 1v10h1V1h2v10h2V1 Z'></path></svg>"
+  """
+  def build_svg(list) do
+    svg_path = path(list)
+    "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 100'><path d='M1 1#{svg_path} Z'></path></svg>"
+  end
+
+end
--- test/doc_test.exs
+++ test/doc_test.exs
@@ -1,4 +1,6 @@
 defmodule DocTest do
   use ExUnit.Case
   doctest BarcoderSvgAlgorithm.Transformer
+  doctest BarcoderSvgAlgorithm.SvgBuilder
+  doctest BarcoderSvgAlgorithm.Group
 end
